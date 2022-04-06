fcf15761dc28d2634a9695f772c606744f9a7c90
nomemolestes <knh3755@naver.com>
2022-04-06 17:42:10 +0900

    [김하늬] rewardsReport dao 생성

---
+++ sakila/src/main/java/dao/RewardDao.java
@@ -0,0 +1,18 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class RewardDao {
+	public Map<String, Object> rewardsReportCall(int minPurchase, int minDollar) {
+		Connection conn = null;
+		CallableStatement = null;
+		ResultSet rs = null;
+		List<Integer> list = new ArrayList<>();
+	}
+	
+
+}
