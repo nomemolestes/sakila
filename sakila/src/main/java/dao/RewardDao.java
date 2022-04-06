f964f635041f12ab722c65b89de66dc180374721
nomemolestes <100700344+nomemolestes@users.noreply.github.com>
2022-04-06 22:07:34 +0900

    [김하늬] 프로시져 rewardsReport dao 수정

--- sakila/src/main/java/dao/RewardDao.java
+++ sakila/src/main/java/dao/RewardDao.java
@@ -11,20 +11,25 @@ import java.util.Map;
 
 import util.DBUtil;
 
-public class RewardDao {
+public class RewardDao { //rewards_report call
 	public Map<String, Object> rewardsReportCall(int minMonthlyPurchase, int minDollarAmountPurchase) {
-		  Map<String, Object> map = new HashMap<String,Object>();
-		  ArrayList<HashMap<String,Object>> list = new ArrayList<HashMap<String,Object>>();
+		  Map<String, Object> map = new HashMap<String,Object>();//해시맵에 데이터를 저장
+		  ArrayList<HashMap<String,Object>> list = new ArrayList<HashMap<String,Object>>();//데이터 저장
+		  //db 초기화
 	      Connection conn = null;
-	      CallableStatement stmt = null; 
+	      CallableStatement stmt = null; //프로시저 실행용 인터페이스
 	      ResultSet rs = null;
+	      //쿼리의 count 변수 초기화
 	      Integer count = 0;
+	      //DBUtil 호출
 	      conn = DBUtil.getConnection();
+	      //try catch
 	      try {
-	         stmt = conn.prepareCall("{CALL rewards_report(?,?,?)}");
-	         stmt.setInt(1, minMonthlyPurchase);
-	         stmt.setDouble(2, minDollarAmountPurchase);
-	         stmt.registerOutParameter(3, Types.INTEGER);
+	    	  //쿼리호출?전송?
+	         stmt = conn.prepareCall("{CALL rewards_report(?,?,?)}");//프로시저 호출하는 메서드
+	         stmt.setInt(1, minMonthlyPurchase);//몇번째 물음표가 무엇인지, 프로시저에 사용할 인자값?이 무엇인지에대해
+	         stmt.setDouble(2, minDollarAmountPurchase);//몇번째 물음표가 무엇인지
+	         stmt.registerOutParameter(3, Types.INTEGER);//프로시저에서 넘어오는값
 	         rs = stmt.executeQuery();
 	         count = stmt.getInt(3);
 			while(rs.next()) {
